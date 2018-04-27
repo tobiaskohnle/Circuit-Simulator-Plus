850ffd2a6afb858a91a42e35ceeba3833daee2cf
tobiaskohnle <tobias_kohnle@web.de>
2018-04-27 20:24:36 +0200

    Initial commit

---
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CircuitSimulatorPlus
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+        void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+        void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
+        }
+    }
+}
