a23f0870d06fc27bcb5428f86a2792fd2577cee5
tobiaskohnle <tobias_kohnle@web.de>
2018-05-21 13:19:46 +0200

    Added documentation

--- CircuitSimulatorPlus/CircuitSimulatorPlus/ConnectionNode.cs
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/ConnectionNode.cs
@@ -8,6 +8,43 @@ namespace CircuitSimulatorPlus
         public bool empty;
         public bool inverted;
         public string name;
-        public ElementaryGate level;
+        /// <summary>
+        /// A reference to the ElementaryGate indicating the current state of this ConnectionNode
+        /// </summary>
+        public ElementaryGate state;
+        /// <summary>
+        /// Represents the ElementaryGate that is interacted with (i.e. used to create connections)
+        /// </summary>
+        public ElementaryGate repr;
+
+        /// <summary>
+        /// True, if this ConnectionNode is displayed as 'high' (1);
+        /// False, if this ConnectionNode is displayed as 'low' (0)
+        /// </summary>
+        public bool State
+        {
+            get { return state.lastState; }
+        }
+        /// <summary>
+        /// True, if this ConnectionNode is NOT connected to another ConnectionNode
+        /// </summary>
+        public bool IsEmpty
+        {
+            get { return empty; }
+        }
+        /// <summary>
+        /// True, if this ConnectionNode is displayed as inverted
+        /// </summary>
+        public bool IsInverted
+        {
+            get { return inverted; }
+        }
+        /// <summary>
+        /// Name displayed next to the ConnectionNode (inside the Gate it is connected to)
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+        }
     }
 }
