44914092170ad4db9cbadf29cbf19f4714deff23
tobiaskohnle <tobias_kohnle@web.de>
2018-05-29 12:24:07 +0200

    Readded Gate

--- CircuitSimulatorPlus/CircuitSimulatorPlus/Gate.cs
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/Gate.cs
@@ -6,31 +6,37 @@ namespace CircuitSimulatorPlus
 {
     public class Gate
     {
-        bool mutable;
-        bool selected;
-        string name;
-        string tag;
-
+        public enum GateType
+        {
+            Custom, And, Or, Not
+        }
         /// <summary>
-        /// InputNodes of the gate
+        /// </summary>
+        public GateType Type { get; private set; }
+        /// <summary>
+        /// The circuit inside of a gate.
+        /// </summary>
+        public List<Gate> Context { get; set; }
+        /// <summary>
+        /// False: elementary gate
+        /// </summary>
+        public bool HasContext { get; private set; }
+        /// <summary>
+        /// InputNodes of the gate.
         /// </summary>
         public List<InputNode> Input { get; set; }
-
         /// <summary>
-        /// OutputNodes of the gate
+        /// OutputNodes of the gate.
         /// </summary>
         public List<OutputNode> Output { get; set; }
-
         /// <summary>
         /// Triggered when any of the output signals changed.
         /// </summary>
         public EventHandler OutputChanged { get; set; }
-
         /// <summary>
         /// Triggered when the Gate's position is changed.
         /// </summary>
         public EventHandler PositionChanged { get; set; }
-
         /// <summary>
         /// Creates the Gate's visual representation on the Render() call.
         /// Render() should only be called once.
@@ -38,58 +44,67 @@ namespace CircuitSimulatorPlus
         public IRenderer Renderer { get; set; }
 
         Point position;
-
+        /// <summary>
+        /// The position on the canvas of the gate.
+        /// </summary>
         public Point Position
         {
             get { return position; }
-
             set {
                 position = value;
                 PositionChanged?.Invoke(this, EventArgs.Empty);
             }
         }
-
         /// <summary>
-        /// Name displayed on top of the gate
+        /// Name displayed on top of the gate.
         /// </summary>
-        public string Name
-        {
-            get { return name; }
-        }
-
+        public string Name { get; set; }
         /// <summary>
-        /// True, if you can add or remove inputs of this gate
+        /// True, if you can add or remove inputs of this gate.
         /// </summary>
-        public bool IsMutable
-        {
-            get { return mutable; }
-        }
-
+        public bool IsMutable { get; set; }
         /// <summary>
-        /// True, if the gate is currently selected
+        /// True, if the gate is currently selected.
         /// </summary>
-        public bool IsSelected
-        {
-            get { return selected; }
-        }
-
+        public bool IsSelected { get; set; }
         /// <summary>
-        /// Tag displayed inside of the gate (e.g ">=1" for OR)
+        /// Tag displayed inside of the gate. (e.g. ">=1" for OR)
         /// </summary>
-        public string Tag
-        {
-            get { return tag; }
-        }
-
+        public string Tag { get; set; }
         /// <summary>
-        /// Moves the gate
+        /// Moves the gate.
         /// </summary>
-        /// <param name="x">Offset along x-axis in units</param>
-        /// <param name="y">Offset along y-axis in units</param>
+        /// <param name="x">Movement along x-axis in units</param>
+        /// <param name="y">Movement along y-axis in units</param>
         public void Move(double x, double y)
         {
             position.X += x;
             position.Y += y;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Eval()
+        {
+            switch (Type)
+            {
+            case GateType.And:
+                foreach (InputNode input in Input)
+                    if (input.State == false)
+                        return false;
+                return true;
+            case GateType.Or:
+                foreach (InputNode input in Input)
+                    if (input.State)
+                        return true;
+                return false;
+            case GateType.Not:
+                foreach (InputNode input in Input)
+                    if (input.State)
+                        return false;
+                return true;
+            }
+            throw new InvalidOperationException($"Can't eval gate of type {Type}.");
         }
     }
 }
