454dea5dfb8618be325084c1cc6720a3d0a5a9d9
tobiaskohnle <tobias_kohnle@web.de>
2018-05-29 12:23:38 +0200

    Readded ConnectionNode

--- CircuitSimulatorPlus/CircuitSimulatorPlus/ConnectionNode.cs
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/ConnectionNode.cs
@@ -5,47 +5,88 @@ namespace CircuitSimulatorPlus
 {
     public abstract class ConnectionNode
     {
-        protected bool empty = true;
-        protected bool inverted;
-        protected string name;
+        public List<ConnectionNode> NextConnectedTo { get; set; }
+        public ConnectionNode BackConnectedTo { get; set; }
 
-        protected List<InputNode> connectedTo = new List<InputNode>();
-
+        bool state;
+        bool stateChanged;
         /// <summary>
-        /// True, if this ConnectionNode is displayed as 'high' (1);
-        /// False, if this ConnectionNode is displayed as 'low' (0)
+        /// True, if this ConnectionNode is displayed as 'high' (1).
+        /// False, if this ConnectionNode is displayed as 'low' (0).
         /// </summary>
         public bool State
         {
-            get { return false/*state.lastState*/; }
+            get { return state; }
+            set {
+                if (state != value)
+                {
+                    stateChanged = !stateChanged;
+                    state = value;
+                }
+            }
         }
         /// <summary>
-        /// True, if this ConnectionNode is NOT connected to another ConnectionNode
+        /// True, if this ConnectionNode is NOT connected to another ConnectionNode.
         /// </summary>
-        public bool IsEmpty
-        {
-            get { return empty; }
-        }
+        public bool IsEmpty { get; set; }
         /// <summary>
-        /// True, if this ConnectionNode is displayed as inverted
+        /// True, if this ConnectionNode is inverted.
         /// </summary>
-        public bool IsInverted
-        {
-            get { return inverted; }
-        }
+        public bool IsInverted { get; set; }
         /// <summary>
-        /// Name displayed next to the ConnectionNode (inside the Gate it is connected to)
+        /// Name displayed next to the ConnectionNode.
+        /// (inside the Gate it is connected to)
         /// </summary>
-        public string Name
-        {
-            get { return name; }
-        }
+        public string Name { get; set; }
 
         public abstract void Clear();
 
+        /// <summary>
+        /// Inverts the ConnectionNode.
+        /// </summary>
         public void Invert()
         {
-            inverted = !inverted;
+            IsInverted = !IsInverted;
+        }
+
+        public void Tick(Queue<ConnectionNode> tickedNodes)
+        {
+            bool nextIsElementary = false;
+            bool lastWasElementary = false;
+            Gate self = null;
+
+            if (IsEmpty)
+            {
+                if (lastWasElementary)
+                {
+                    State = self.Eval();
+                }
+            }
+            else
+            {
+                State = BackConnectedTo.State != IsInverted;
+            }
+
+            if (stateChanged)
+            {
+                stateChanged = false;
+
+                if (nextIsElementary)
+                {
+                    foreach (OutputNode output in self.Output)
+                    {
+                        foreach (ConnectionNode node in output.NextConnectedTo)
+                        {
+                            tickedNodes.Enqueue(node);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (ConnectionNode node in NextConnectedTo)
+                        tickedNodes.Enqueue(node);
+                }
+            }
         }
     }
 }
