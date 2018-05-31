ff1c7c43076e4654b3d2e0c9bb44c02190e1e433
BlueJellyFish <BlueJellyFish@MSI>
2018-05-31 14:20:23 +0200

    created DeleteGateAction

---
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/DeleteGateAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CircuitSimulatorPlus
+{
+   public class DeleteGateAction : Action
+    {
+        List<Gate> createdGates;
+        Gate gate;
+        Gate.GateType gateType;
+        Point position;
+
+        public DeleteGateAction(Gate gate, Gate.GateType gateType, Point position, List<Gate> createdGates, string message) : base(message)
+        {
+            this.gate = gate;
+            this.gateType = gateType;
+            this.position = position;
+        }
+        public override void Redo()
+        {
+            gate.Renderer.Unrender();
+            createdGates.Remove(gate);
+        }
+
+        public override void Undo()
+        {
+            gate.Renderer.Render();
+            createdGates.Add(gate);
+        }
+    }
+}
