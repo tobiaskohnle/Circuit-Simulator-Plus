90a2c4e2f7c2c8711daf32fa83563b77e3c6a519
BlueJellyFish <BlueJellyFish@MSI>
2018-05-31 12:52:42 +0200

    GateCreationAction created (unfinished)

---
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/GateCreateAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitSimulatorPlus
+{
+   public class GateCreateAction : Action
+    {
+        Gate gate;
+        Gate.GateType gateType;
+        Point position;
+
+        public GateCreateAction(Gate gate, Gate.GateType gateType)
+        {
+            this.gate = gate;
+            this.gateType = gateType;
+        }
+         public override void Redo()
+        {
+            
+        }
+
+        public override void Undo()
+        {
+
+        }
+    }
+}
