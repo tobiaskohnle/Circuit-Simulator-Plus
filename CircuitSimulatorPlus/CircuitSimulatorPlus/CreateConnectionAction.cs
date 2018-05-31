5abe6079d8467c43a790c577293087babc00a12c
BlueJellyFish <BlueJellyFish@MSI>
2018-05-31 15:07:14 +0200

    created CreateConnectionAction.cs (unfinished -> waiting for disconnect-function)

---
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/CreateConnectionAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitSimulatorPlus
+{
+   public class CreateConnectionAction : Action
+    {
+        ConnectionNode firstNode, secondNode;
+
+        public CreateConnectionAction(ConnectionNode firstNode,ConnectionNode secondNode,string message) : base(message)
+        {
+            this.firstNode = firstNode;
+            this.secondNode = secondNode;
+        }
+        public override void Redo()
+        {
+            firstNode.ConnectTo(secondNode);
+        }
+        
+        public override void Undo()
+        {
+            //waiting for disconnect-function
+        }        
+    }
+}
