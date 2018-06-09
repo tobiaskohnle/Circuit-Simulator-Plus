2029e9f586d3a0e48081b49b72183798a05b4689
tobiaskohnle <tobias_kohnle@web.de>
2018-06-09 16:56:02 +0200

    Added DropOutStack

---
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/Misc/DropOutStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CircuitSimulatorPlus
+{
+    public class DropOutStack<T>
+    {
+        T[] elements;
+        int index;
+        int count;
+
+        public DropOutStack(int bufferSize)
+        {
+            elements = new T[bufferSize];
+        }
+
+        public void Push(T item)
+        {
+            if (count < elements.Length)
+                count++;
+
+            if (++index >= elements.Length)
+                index = 0;
+
+            elements[index] = item;
+        }
+
+        public T Pop()
+        {
+            if (--index < 0)
+                index = elements.Length - 1;
+
+            return Peek();
+        }
+
+        public T Peek()
+        {
+            if (count > 0)
+            {
+                return elements[index];
+            }
+            throw new InvalidOperationException("The stack is empty.");
+        }
+
+        public void Clear()
+        {
+            count = 0;
+        }
+
+        public int Count
+        {
+            get {
+                return count;
+            }
+        }
+    }
+}
