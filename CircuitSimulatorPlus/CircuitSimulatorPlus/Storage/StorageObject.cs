e437996fcd823a1b4308eea0211455f5d802b1f7
tobiaskohnle <tobias_kohnle@web.de>
2018-06-12 17:10:51 +0200

    Changed fields to properties

--- CircuitSimulatorPlus/CircuitSimulatorPlus/Storage/StorageObject.cs
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/Storage/StorageObject.cs
@@ -9,16 +9,16 @@ namespace CircuitSimulatorPlus
 {
     public class StorageObject
     {
-        public string Name;
-        public Point Position;
-        public string Type;
-        public List<StorageObject> Context;
-        public int[] InnerInputConnections;
-        public int[] InnerOutputConnections;
-        public int[] InputConnections;
-        public int[] OutputConnections;
-        public List<int> InvertedInputs;
-        public List<int> InvertedOutputs;
-        public List<int> InitialActiveOutputs;
+        public string Name { get; set; }
+        public Point Position { get; set; }
+        public string Type { get; set; }
+        public List<StorageObject> Context { get; set; }
+        public int[] InnerInputConnections { get; set; }
+        public int[] InnerOutputConnections { get; set; }
+        public int[] InputConnections { get; set; }
+        public int[] OutputConnections { get; set; }
+        public List<int> InvertedInputs { get; set; }
+        public List<int> InvertedOutputs { get; set; }
+        public List<int> InitialActiveOutputs { get; set; }
     }
 }
