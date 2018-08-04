b3a90c31c48dc2a046898ec84565de03a32f9cda
BlueJellyFish <BlueJellyFish@MSI>
2018-08-04 16:36:45 +0200

    Created DeleteInputAction.cs, created ToggleObjectsAction.cs

---
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/Actions/DeleteInputAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitSimulatorPlus.Actions
+{
+    class DeleteInputAction : Action
+    {
+        public DeleteInputAction(List<ConnectionNode> List) : base("Delete Input")
+        {
+           
+        }
+
+        public override void Redo()
+        {
+            
+        }
+
+        public override void Undo()
+        {
+            
+        }
+    }
+}
