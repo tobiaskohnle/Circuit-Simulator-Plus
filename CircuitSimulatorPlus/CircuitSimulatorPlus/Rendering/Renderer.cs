f6b7893bdd9d1604fb20b8d35421bddc0dbc3ca5
tobiaskohnle <tobias_kohnle@web.de>
2018-08-19 19:40:11 +0200

    Added Renderer

---
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/Rendering/Renderer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitSimulatorPlus
+{
+    public abstract class Renderer<T>
+    {
+        public delegate void ValueChanged(T state);
+    }
+}
