6e27c19eb4bcf202c3b9d5b9b3c35b61e0856f8b
tobiaskohnle <tobias_kohnle@web.de>
2018-09-01 21:35:13 +0200

    Updated LineHitbox

--- CircuitSimulatorPlus/CircuitSimulatorPlus/Objects/Hitboxes/LineHitbox.cs
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/Objects/Hitboxes/LineHitbox.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CircuitSimulatorPlus
 {
     public class LineHitbox : Hitbox
     {
+        public LineHitbox(double distanceFactor) : base(distanceFactor)
+        {
+        }
+
+        public override Rect RectBounds
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override double DistanceTo(Point pos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IncludesPos(Point pos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsIncludedIn(Rect rect)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
