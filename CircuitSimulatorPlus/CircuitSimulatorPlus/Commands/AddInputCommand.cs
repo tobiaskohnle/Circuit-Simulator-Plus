255f129e2f8b04a625f09066e0cbaed62323ddd4
BlueJellyFish <BlueJellyFish@MSI>
2018-09-01 14:20:41 +0200

    Finished AddInputCommand.cs and DeleteInpiutCommand.cs

--- CircuitSimulatorPlus/CircuitSimulatorPlus/Commands/AddInputCommand.cs
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/Commands/AddInputCommand.cs
@@ -8,19 +8,24 @@ namespace CircuitSimulatorPlus
 {
     public class AddInputCommand : Command
     {
+        Gate Gate;
         public AddInputCommand(Gate gate) : base("Added input to gate")
         {
-
+            this.Gate = gate;
         }
 
         public override void Redo()
         {
-
+            var inputNode = new InputNode(Gate);
+            Gate.Input.Add(inputNode);
+            MainWindow.Self.ClickableObjects.Add(inputNode);
         }
 
         public override void Undo()
         {
-
+            var inputNode = new InputNode(Gate);
+            Gate.Input.Remove(inputNode);
+            MainWindow.Self.ClickableObjects.Remove(inputNode);
         }
     }
 }
