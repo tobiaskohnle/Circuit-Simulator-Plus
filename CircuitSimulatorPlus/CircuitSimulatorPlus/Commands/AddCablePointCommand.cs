160e60730370c598b1108a09f00971c486df9d64
tobiaskohnle <tobias_kohnle@web.de>
2018-09-03 14:22:35 +0200

    Added AddCablePointCommand

---
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/Commands/AddCablePointCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CircuitSimulatorPlus
+{
+    public class AddCablePointCommand : Command
+    {
+        public AddCablePointCommand() : base("Add Cable Point")
+        {
+        }
+
+        public override void Redo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Undo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
