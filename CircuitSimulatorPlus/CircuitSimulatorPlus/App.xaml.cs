fab9c716bf7537d5bf20304e66ad797e0cbb77d3
tobiaskohnle <tobias_kohnle@web.de>
2018-09-04 22:53:04 +0200

    Added Application_DispatcherUnhandledException

--- CircuitSimulatorPlus/CircuitSimulatorPlus/App.xaml.cs
+++ CircuitSimulatorPlus/CircuitSimulatorPlus/App.xaml.cs
@@ -5,10 +5,15 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace CircuitSimulatorPlus
 {
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString());
+        }
     }
 }
